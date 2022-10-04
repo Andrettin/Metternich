443eecc22200fece957848a3ca095b8b427bac16
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-04 18:36:24 +0200

    Added basic province class definition

--- src/map/province.h
+++ src/map/province.h
@@ -1,6 +1,22 @@
 #pragma once
 
-#include "pch.h"
+#include "database/data_type.h"
+#include "database/named_data_entry.h"
 
 namespace metternich {
+
+class province final : public named_data_entry, public data_type<province>
+{
+	Q_OBJECT
+
+public:
+	static constexpr const char class_identifier[] = "province";
+	static constexpr const char property_class_identifier[] = "metternich::province*";
+	static constexpr const char database_folder[] = "provinces";
+
+	explicit province(const std::string &identifier) : named_data_entry(identifier)
+	{
+	}
+};
+
 }
