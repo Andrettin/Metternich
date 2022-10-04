a492eaf0984283c474b4646a81519dcbea81023d
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-04 20:52:21 +0200

    Added color property for provinces

--- src/map/province.cpp
+++ src/map/province.cpp
@@ -1,3 +1,14 @@
 #include "metternich.h"
 
 #include "province.h"
+
+#include "util/assert_util.h"
+
+namespace metternich {
+
+void province::check() const
+{
+	assert_throw(this->get_color().isValid());
+}
+
+}
--- src/map/province.h
+++ src/map/province.h
@@ -2,6 +2,7 @@
 
 #include "database/data_type.h"
 #include "database/named_data_entry.h"
+#include "util/color_container.h"
 
 namespace metternich {
 
@@ -9,14 +10,71 @@ class province final : public named_data_entry, public data_type<province>
 {
 	Q_OBJECT
 
+	Q_PROPERTY(QColor color READ get_color WRITE set_color)
+
 public:
 	static constexpr const char class_identifier[] = "province";
 	static constexpr const char property_class_identifier[] = "metternich::province*";
 	static constexpr const char database_folder[] = "provinces";
 
+	static province *get_by_color(const QColor &color)
+	{
+		province *province = province::try_get_by_color(color);
+
+		if (province == nullptr) {
+			throw std::runtime_error("No province found for color: (" + std::to_string(color.red()) + ", " + std::to_string(color.green()) + ", " + std::to_string(color.blue()) + ").");
+		}
+
+		return province;
+	}
+
+	static province *try_get_by_color(const QColor &color)
+	{
+		const auto find_iterator = province::provinces_by_color.find(color);
+		if (find_iterator != province::provinces_by_color.end()) {
+			return find_iterator->second;
+		}
+
+		return nullptr;
+	}
+
+	static void clear()
+	{
+		data_type::clear();
+		province::provinces_by_color.clear();
+	}
+private:
+	static inline color_map<province *> provinces_by_color;
+
+public:
+
 	explicit province(const std::string &identifier) : named_data_entry(identifier)
 	{
 	}
+
+	virtual void check() const override;
+
+	const QColor &get_color() const
+	{
+		return this->color;
+	}
+
+	void set_color(const QColor &color)
+	{
+		if (color == this->get_color()) {
+			return;
+		}
+
+		if (province::try_get_by_color(color) != nullptr) {
+			throw std::runtime_error("Color is already used by another province.");
+		}
+
+		this->color = color;
+		province::provinces_by_color[color] = this;
+	}
+
+private:
+	QColor color;
 };
 
 }
