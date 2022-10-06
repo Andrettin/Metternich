810c205cd73bec8770e4fb41ee6a4c9dcb836891
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-06 20:17:59 +0200

    Added support for countries having a capital province, along with a list of provinces for them for random maps

--- src/country/country.cpp
+++ src/country/country.cpp
@@ -3,6 +3,7 @@
 #include "country/country.h"
 
 #include "country/country_type.h"
+#include "map/province.h"
 #include "util/assert_util.h"
 
 namespace metternich {
@@ -12,8 +13,23 @@ country::country(const std::string &identifier)
 {
 }
 
+void country::process_gsml_scope(const gsml_data &scope)
+{
+	const std::string &tag = scope.get_tag();
+	const std::vector<std::string> &values = scope.get_values();
+
+	if (tag == "provinces") {
+		for (const std::string &value : values) {
+			this->provinces.push_back(province::get(value));
+		}
+	} else {
+		data_entry::process_gsml_scope(scope);
+	}
+}
+
 void country::check() const
 {
+	assert_throw(this->get_capital_province() != nullptr);
 	assert_throw(this->get_color().isValid());
 }
 
--- src/country/country.h
+++ src/country/country.h
@@ -5,6 +5,7 @@
 
 namespace metternich {
 
+class province;
 enum class country_type;
 
 class country final : public named_data_entry, public data_type<country>
@@ -13,6 +14,7 @@ class country final : public named_data_entry, public data_type<country>
 
 	Q_PROPERTY(metternich::country_type type MEMBER type READ get_type)
 	Q_PROPERTY(QColor color MEMBER color READ get_color)
+	Q_PROPERTY(metternich::province* capital_province MEMBER capital_province)
 
 public:
 	static constexpr const char class_identifier[] = "country";
@@ -21,6 +23,7 @@ public:
 
 	explicit country(const std::string &identifier);
 
+	virtual void process_gsml_scope(const gsml_data &scope) override;
 	virtual void check() const override;
 
 	country_type get_type() const
@@ -33,9 +36,21 @@ public:
 		return this->color;
 	}
 
+	const province *get_capital_province() const
+	{
+		return this->capital_province;
+	}
+
+	const std::vector<const province *> &get_provinces() const
+	{
+		return this->provinces;
+	}
+
 private:
 	country_type type;
 	QColor color;
+	province *capital_province = nullptr;
+	std::vector<const province *> provinces; //provinces for this country when it is generated in random maps
 };
 
 }
