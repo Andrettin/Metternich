5f54bd30f64506c21a3531c403231246427aba84
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-16 18:53:04 +0200

    Added required technology and discovery technology fields for resources

--- src/economy/resource.h
+++ src/economy/resource.h
@@ -6,6 +6,7 @@
 namespace metternich {
 
 class commodity;
+class technology;
 
 //resources are present on tiles, allowing the tile to produce a given commodity
 //multiple resources can produce the same commodity
@@ -15,6 +16,8 @@ class resource final : public named_data_entry, public data_type<resource>
 
 	Q_PROPERTY(metternich::commodity* commodity MEMBER commodity)
 	Q_PROPERTY(std::filesystem::path icon_filepath MEMBER icon_filepath WRITE set_icon_filepath)
+	Q_PROPERTY(metternich::technology* required_technology MEMBER required_technology)
+	Q_PROPERTY(metternich::technology* discovery_technology MEMBER discovery_technology)
 
 public:
 	static constexpr const char class_identifier[] = "resource";
@@ -39,9 +42,21 @@ public:
 
 	void set_icon_filepath(const std::filesystem::path &filepath);
 
+	const technology *get_required_technology() const
+	{
+		return this->required_technology;
+	}
+
+	const technology *get_discovery_technology() const
+	{
+		return this->discovery_technology;
+	}
+
 private:
 	metternich::commodity *commodity = nullptr;
 	std::filesystem::path icon_filepath;
+	technology *required_technology = nullptr; //technology which is required to see the resource on the tile
+	technology *discovery_technology = nullptr; //technology which is obtained when exploring this resource tile
 };
 
 }
