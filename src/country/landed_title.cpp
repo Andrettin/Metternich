7710392a38403587c04be280f66c8ff1f94f98f7
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-20 20:06:05 +0200

    Added support for title-specific names and character names for landed titles

--- src/country/landed_title.cpp
+++ src/country/landed_title.cpp
@@ -5,15 +5,66 @@
 #include "country/landed_title_tier.h"
 #include "map/site.h"
 #include "util/assert_util.h"
+#include "util/gender.h"
 
 namespace metternich {
 
 //must be initialized after sites, in their initialization function landed titles may be created
 const std::set<std::string> landed_title::database_dependencies = { site::class_identifier };
 
+void landed_title::process_title_names(title_name_map &title_names, const gsml_data &scope)
+{
+	scope.for_each_property([&](const gsml_property &property) {
+		const std::string &key = property.get_key();
+		const std::string &value = property.get_value();
+		const landed_title_tier tier = enum_converter<landed_title_tier>::to_enum(key);
+		title_names[tier] = value;
+	});
+}
+
+void landed_title::process_character_title_names(character_title_name_map &character_title_names, const gsml_data &scope)
+{
+	scope.for_each_property([&](const gsml_property &property) {
+		const std::string &key = property.get_key();
+		const std::string &value = property.get_value();
+		const landed_title_tier tier = enum_converter<landed_title_tier>::to_enum(key);
+		character_title_names[tier][gender::none] = value;
+	});
+
+	scope.for_each_child([&](const gsml_data &child_scope) {
+		const landed_title_tier tier = enum_converter<landed_title_tier>::to_enum(child_scope.get_tag());
+
+		landed_title::process_character_title_name_scope(character_title_names[tier], child_scope);
+	});
+}
+
+void landed_title::process_character_title_name_scope(std::map<gender, std::string> &character_title_names, const gsml_data &scope)
+{
+	scope.for_each_property([&](const gsml_property &property) {
+		const std::string &key = property.get_key();
+		const std::string &value = property.get_value();
+		const gender gender = enum_converter<archimedes::gender>::to_enum(key);
+		character_title_names[gender] = value;
+	});
+}
+
 landed_title::landed_title(const std::string &identifier)
 	: named_data_entry(identifier), default_tier(landed_title_tier::none), min_tier(landed_title_tier::none), max_tier(landed_title_tier::none)
 {
+}
+
+void landed_title::process_gsml_scope(const gsml_data &scope)
+{
+	const std::string &tag = scope.get_tag();
+	const std::vector<std::string> &values = scope.get_values();
+
+	if (tag == "title_names") {
+		landed_title::process_title_names(this->title_names, scope);
+	} else if (tag == "character_title_names") {
+		landed_title::process_character_title_names(this->character_title_names, scope);
+	} else {
+		data_entry::process_gsml_scope(scope);
+	}
 }
 
 void landed_title::initialize()
--- src/country/landed_title.h
+++ src/country/landed_title.h
@@ -3,6 +3,10 @@
 #include "database/data_type.h"
 #include "database/named_data_entry.h"
 
+namespace archimedes {
+	enum class gender;
+}
+
 namespace metternich {
 
 class country;
@@ -18,14 +22,22 @@ class landed_title final : public named_data_entry, public data_type<landed_titl
 	Q_PROPERTY(metternich::landed_title_tier max_tier MEMBER max_tier READ get_max_tier)
 
 public:
+	using title_name_map = std::map<landed_title_tier, std::string>;
+	using character_title_name_map = std::map<landed_title_tier, std::map<gender, std::string>>;
+
 	static constexpr const char class_identifier[] = "landed_title";
 	static constexpr const char property_class_identifier[] = "metternich::landed_title*";
 	static constexpr const char database_folder[] = "landed_titles";
 
 	static const std::set<std::string> database_dependencies;
 
+	static void process_title_names(title_name_map &title_names, const gsml_data &scope);
+	static void process_character_title_names(character_title_name_map &character_title_names, const gsml_data &scope);
+	static void process_character_title_name_scope(std::map<gender, std::string> &character_title_names, const gsml_data &scope);
+
 	explicit landed_title(const std::string &identifier);
 
+	virtual void process_gsml_scope(const gsml_data &scope) override;
 	virtual void initialize() override;
 	virtual void check() const override;
 
@@ -67,6 +79,8 @@ private:
 	landed_title_tier default_tier;
 	landed_title_tier min_tier;
 	landed_title_tier max_tier;
+	title_name_map title_names;
+	character_title_name_map character_title_names;
 };
 
 }
