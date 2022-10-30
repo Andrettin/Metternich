13c29c361498266a4cf248d24f407e68874bf9e1
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-30 20:46:23 +0100

    Catch and report exceptions for map_template::write_province_image()

--- src/map/map_template.cpp
+++ src/map/map_template.cpp
@@ -12,6 +12,7 @@
 #include "map/terrain_type.h"
 #include "map/world.h"
 #include "util/assert_util.h"
+#include "util/exception_util.h"
 #include "util/geoshape_util.h"
 #include "util/path_util.h"
 #include "util/point_util.h"
@@ -205,38 +206,43 @@ void map_template::write_province_image()
 {
 	using province_geodata_map_type = province_map<std::vector<std::unique_ptr<QGeoShape>>>;
 
-	assert_throw(this->get_world() != nullptr);
+	try {
+		assert_throw(this->get_world() != nullptr);
 
-	province_geodata_map_type province_geodata_map = this->get_world()->parse_provinces_geojson_folder();
+		province_geodata_map_type province_geodata_map = this->get_world()->parse_provinces_geojson_folder();
 
-	color_map<std::vector<std::unique_ptr<QGeoShape>>> geodata_map;
+		color_map<std::vector<std::unique_ptr<QGeoShape>>> geodata_map;
 
-	for (auto &[province, geoshapes] : province_geodata_map) {
-		const QColor color = province->get_color();
-		if (!color.isValid()) {
-			throw std::runtime_error("Province \"" + province->get_identifier() + "\" has no valid color.");
+		for (auto &[province, geoshapes] : province_geodata_map) {
+			const QColor color = province->get_color();
+			if (!color.isValid()) {
+				throw std::runtime_error("Province \"" + province->get_identifier() + "\" has no valid color.");
+			}
+
+			vector::merge(geodata_map[color], std::move(geoshapes));
 		}
 
-		vector::merge(geodata_map[color], std::move(geoshapes));
+		assert_throw(this->map_projection != nullptr);
+
+		this->map_projection->validate_area(this->get_georectangle(), this->get_size());
+
+		QImage base_image;
+
+		if (!this->get_province_image_filepath().empty()) {
+			base_image = QImage(path::to_qstring(this->get_province_image_filepath()));
+			assert_throw(!base_image.isNull());
+		}
+
+		std::filesystem::path output_filepath = this->get_province_image_filepath().filename();
+		if (output_filepath.empty()) {
+			output_filepath = "provinces.png";
+		}
+
+		geoshape::write_image(output_filepath, geodata_map, this->get_georectangle(), this->get_size(), this->map_projection, base_image, this->geocoordinate_x_offset);
+	} catch (const std::exception &exception) {
+		exception::report(exception);
+		std::terminate();
 	}
-
-	assert_throw(this->map_projection != nullptr);
-
-	this->map_projection->validate_area(this->get_georectangle(), this->get_size());
-
-	QImage base_image;
-
-	if (!this->get_province_image_filepath().empty()) {
-		base_image = QImage(path::to_qstring(this->get_province_image_filepath()));
-		assert_throw(!base_image.isNull());
-	}
-
-	std::filesystem::path output_filepath = this->get_province_image_filepath().filename();
-	if (output_filepath.empty()) {
-		output_filepath = "provinces.png";
-	}
-
-	geoshape::write_image(output_filepath, geodata_map, this->get_georectangle(), this->get_size(), this->map_projection, base_image, this->geocoordinate_x_offset);
 }
 
 void map_template::apply() const
