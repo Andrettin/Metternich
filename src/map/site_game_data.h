eff3aaa2a7dbbc32363c4b00fab73453e73d5eb0
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-11-09 09:11:50 +0100

    Exposed the site game data province to QML

--- src/map/site_game_data.h
+++ src/map/site_game_data.h
@@ -13,6 +13,7 @@ class site_game_data final : public QObject
 
 	Q_PROPERTY(QPoint tile_pos READ get_tile_pos NOTIFY tile_pos_changed)
 	Q_PROPERTY(QString current_cultural_name READ get_current_cultural_name_qstring NOTIFY culture_changed)
+	Q_PROPERTY(metternich::province *province READ get_province_unconst NOTIFY tile_pos_changed)
 
 public:
 	explicit site_game_data(const site *site) : site(site)
@@ -42,6 +43,15 @@ public:
 	}
 
 	const province *get_province() const;
+
+private:
+	//for the Qt property (pointers there can't be const)
+	province *get_province_unconst() const
+	{
+		return const_cast<province *>(this->get_province());
+	}
+
+public:
 	const culture *get_culture() const;
 
 	const std::string &get_current_cultural_name() const;
