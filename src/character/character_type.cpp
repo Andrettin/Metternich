f776cf1f39320d3f898fc58f166cfc350eee85cd
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-01-10 17:39:55 +0100

    Each character type now has a primary attribute

--- src/character/character_type.cpp
+++ src/character/character_type.cpp
@@ -2,14 +2,21 @@
 
 #include "character/character_type.h"
 
+#include "character/attribute.h"
 #include "country/culture.h"
 #include "util/assert_util.h"
 
 namespace metternich {
 
+character_type::character_type(const std::string &identifier)
+	: named_data_entry(identifier), primary_attribute(attribute::none)
+{
+}
+
 void character_type::check() const
 {
 	assert_throw(this->get_portrait() != nullptr);
+	assert_throw(this->get_primary_attribute() != attribute::none);
 }
 
 }
--- src/character/character_type.h
+++ src/character/character_type.h
@@ -6,21 +6,21 @@
 namespace metternich {
 
 class icon;
+enum class attribute;
 
 class character_type final : public named_data_entry, public data_type<character_type>
 {
 	Q_OBJECT
 
 	Q_PROPERTY(metternich::icon* portrait MEMBER portrait NOTIFY changed)
+	Q_PROPERTY(metternich::attribute primary_attribute MEMBER primary_attribute NOTIFY changed)
 
 public:
 	static constexpr const char class_identifier[] = "character_type";
 	static constexpr const char property_class_identifier[] = "metternich::character_type*";
 	static constexpr const char database_folder[] = "character_types";
 
-	explicit character_type(const std::string &identifier) : named_data_entry(identifier)
-	{
-	}
+	explicit character_type(const std::string &identifier);
 
 	virtual void check() const override;
 
@@ -29,11 +29,17 @@ public:
 		return this->portrait;
 	}
 
+	attribute get_primary_attribute() const
+	{
+		return this->primary_attribute;
+	}
+
 signals:
 	void changed();
 
 private:
 	icon *portrait = nullptr;
+	attribute primary_attribute;
 };
 
 }
