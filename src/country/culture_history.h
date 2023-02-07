5d8e39eefe8a914bc600dab262f6a187d492c737
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-02-07 07:59:34 +0100

    Fix to previous commit

--- src/country/culture_history.h
+++ src/country/culture_history.h
@@ -10,26 +10,26 @@ class culture_history final : public data_entry_history
 {
 	Q_OBJECT
 
-	Q_PROPERTY(std::vector<const technology *> technologies READ get_technologies)
+	Q_PROPERTY(std::vector<metternich::technology *> technologies READ get_technologies)
 
 public:
-	const std::vector<const technology *> &get_technologies() const
+	const std::vector<technology *> &get_technologies() const
 	{
 		return this->technologies;
 	}
 
-	Q_INVOKABLE void add_technology(const technology *technology)
+	Q_INVOKABLE void add_technology(technology *technology)
 	{
 		this->technologies.push_back(technology);
 	}
 
-	Q_INVOKABLE void remove_technology(const technology *technology)
+	Q_INVOKABLE void remove_technology(technology *technology)
 	{
 		std::erase(this->technologies, technology);
 	}
 
 private:
-	std::vector<const technology *> technologies;
+	std::vector<technology *> technologies;
 };
 
 }
