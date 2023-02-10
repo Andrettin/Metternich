268933c7aa02d57029d7b9555071da9d25327687
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-02-10 18:19:18 +0100

    Added support for pre-scripted effects

---
+++ src/script/effect/scripted_effect.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include "database/data_entry.h"
+#include "database/data_type.h"
+#include "script/effect/effect_list.h"
+
+namespace metternich {
+
+class character;
+class country;
+class population_unit;
+class province;
+
+template <typename scope_type>
+class effect_list;
+
+//the class for a predefined, reusable scripted effect
+template <typename scope_type>
+class scripted_effect_base
+{
+public:
+	void process_gsml_property(const gsml_property &property)
+	{
+		this->effects.process_gsml_property(property);
+	}
+
+	void process_gsml_scope(const gsml_data &scope)
+	{
+		this->effects.process_gsml_scope(scope);
+	}
+
+	void check() const
+	{
+		this->get_effects().check();
+	}
+
+	virtual const std::string &get_identifier() const = 0;
+
+	const effect_list<scope_type> &get_effects() const
+	{
+		return this->effects;
+	}
+
+private:
+	effect_list<scope_type> effects;
+};
+
+class character_scripted_effect final : public data_entry, public data_type<character_scripted_effect>, public scripted_effect_base<const character>
+{
+	Q_OBJECT
+
+public:
+	static constexpr const char *class_identifier = "character_scripted_effect";
+	static constexpr const char property_class_identifier[] = "metternich::character_scripted_effect*";
+	static constexpr const char *database_folder = "scripted_effects/character";
+
+	explicit character_scripted_effect(const std::string &identifier) : data_entry(identifier)
+	{
+	}
+
+	virtual void process_gsml_property(const gsml_property &property) override
+	{
+		scripted_effect_base::process_gsml_property(property);
+	}
+
+	virtual void process_gsml_scope(const gsml_data &scope) override
+	{
+		scripted_effect_base::process_gsml_scope(scope);
+	}
+
+	virtual void check() const override
+	{
+		scripted_effect_base::check();
+	}
+
+	virtual const std::string &get_identifier() const override
+	{
+		return data_entry::get_identifier();
+	}
+};
+
+class country_scripted_effect final : public data_entry, public data_type<country_scripted_effect>, public scripted_effect_base<const country>
+{
+	Q_OBJECT
+
+public:
+	static constexpr const char *class_identifier = "country_scripted_effect";
+	static constexpr const char property_class_identifier[] = "metternich::country_scripted_effect*";
+	static constexpr const char *database_folder = "scripted_effects/country";
+
+	explicit country_scripted_effect(const std::string &identifier) : data_entry(identifier)
+	{
+	}
+
+	virtual void process_gsml_property(const gsml_property &property) override
+	{
+		scripted_effect_base::process_gsml_property(property);
+	}
+
+	virtual void process_gsml_scope(const gsml_data &scope) override
+	{
+		scripted_effect_base::process_gsml_scope(scope);
+	}
+
+	virtual void check() const override
+	{
+		scripted_effect_base::check();
+	}
+
+	virtual const std::string &get_identifier() const override
+	{
+		return data_entry::get_identifier();
+	}
+};
+
+class population_unit_scripted_effect final : public data_entry, public data_type<population_unit_scripted_effect>, public scripted_effect_base<population_unit>
+{
+	Q_OBJECT
+
+public:
+	static constexpr const char *class_identifier = "population_unit_scripted_effect";
+	static constexpr const char property_class_identifier[] = "metternich::population_unit_scripted_effect*";
+	static constexpr const char *database_folder = "scripted_effects/population_unit";
+
+	explicit population_unit_scripted_effect(const std::string &identifier) : data_entry(identifier)
+	{
+	}
+
+	virtual void process_gsml_property(const gsml_property &property) override
+	{
+		scripted_effect_base::process_gsml_property(property);
+	}
+
+	virtual void process_gsml_scope(const gsml_data &scope) override
+	{
+		scripted_effect_base::process_gsml_scope(scope);
+	}
+
+	virtual void check() const override
+	{
+		scripted_effect_base::check();
+	}
+
+	virtual const std::string &get_identifier() const override
+	{
+		return data_entry::get_identifier();
+	}
+};
+
+class province_scripted_effect final : public data_entry, public data_type<province_scripted_effect>, public scripted_effect_base<const province>
+{
+	Q_OBJECT
+
+public:
+	static constexpr const char *class_identifier = "province_scripted_effect";
+	static constexpr const char property_class_identifier[] = "metternich::province_scripted_effect*";
+	static constexpr const char *database_folder = "scripted_effects/province";
+
+	explicit province_scripted_effect(const std::string &identifier) : data_entry(identifier)
+	{
+	}
+
+	virtual void process_gsml_property(const gsml_property &property) override
+	{
+		scripted_effect_base::process_gsml_property(property);
+	}
+
+	virtual void process_gsml_scope(const gsml_data &scope) override
+	{
+		scripted_effect_base::process_gsml_scope(scope);
+	}
+
+	virtual void check() const override
+	{
+		scripted_effect_base::check();
+	}
+
+	virtual const std::string &get_identifier() const override
+	{
+		return data_entry::get_identifier();
+	}
+};
+
+}
---
+++ src/script/effect/scripted_effect_effect.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "script/effect/effect.h"
+#include "script/effect/scripted_effect.h"
+#include "util/assert_util.h"
+
+namespace metternich {
+
+template <typename scope_type>
+class scripted_effect_base;
+
+template <typename scope_type>
+class scripted_effect_effect final : public effect<scope_type>
+{
+public:
+	explicit scripted_effect_effect(const std::string &effect_identifier, const gsml_operator effect_operator) : effect<scope_type>(effect_operator)
+	{
+		if constexpr (std::is_same_v<scope_type, const character>) {
+			this->scripted_effect = character_scripted_effect::get(effect_identifier);
+		} else if constexpr (std::is_same_v<scope_type, const country>) {
+			this->scripted_effect = country_scripted_effect::get(effect_identifier);
+		} else if constexpr (std::is_same_v<scope_type, population_unit>) {
+			this->scripted_effect = population_unit_scripted_effect::get(effect_identifier);
+		} else if constexpr (std::is_same_v<scope_type, const province>) {
+			this->scripted_effect = province_scripted_effect::get(effect_identifier);
+		} else {
+			assert_throw(false);
+		}
+	}
+
+	virtual const std::string &get_class_identifier() const override
+	{
+		static const std::string class_identifier = "scripted_effect";
+		return class_identifier;
+	}
+
+	virtual void do_assignment_effect(scope_type *scope, context &ctx) const override
+	{
+		this->scripted_effect->get_effects().do_effects(scope, ctx);
+	}
+
+	virtual std::string get_assignment_string(const scope_type *scope, const read_only_context &ctx, const size_t indent, const std::string &prefix) const override
+	{
+		return this->scripted_effect->get_effects().get_effects_string(scope, ctx, indent, prefix);
+	}
+
+private:
+	const scripted_effect_base<scope_type> *scripted_effect = nullptr;
+};
+
+}
