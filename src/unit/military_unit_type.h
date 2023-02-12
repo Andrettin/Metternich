f988371b65521f54c141c5f43e04495b4c187680
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-02-12 19:57:46 +0100

    Added Bowmen and Spear Cavalry military unit categories

--- src/unit/military_unit_type.h
+++ src/unit/military_unit_type.h
@@ -20,8 +20,8 @@ class military_unit_type final : public named_data_entry, public data_type<milit
 	Q_PROPERTY(metternich::culture* culture MEMBER culture NOTIFY changed)
 	Q_PROPERTY(metternich::cultural_group* cultural_group MEMBER cultural_group NOTIFY changed)
 	Q_PROPERTY(metternich::icon* icon MEMBER icon NOTIFY changed)
-	Q_PROPERTY(int melee MEMBER melee READ get_melee NOTIFY changed)
 	Q_PROPERTY(int firepower MEMBER firepower READ get_firepower NOTIFY changed)
+	Q_PROPERTY(int melee MEMBER melee READ get_melee NOTIFY changed)
 	Q_PROPERTY(int range MEMBER range READ get_range NOTIFY changed)
 	Q_PROPERTY(int defense MEMBER defense READ get_defense NOTIFY changed)
 	Q_PROPERTY(int movement MEMBER movement READ get_movement NOTIFY changed)
@@ -64,14 +64,14 @@ public:
 		return this->icon;
 	}
 
-	int get_melee() const
-	{
-		return this->melee;
-	}
-
 	int get_firepower() const
 	{
 		return this->firepower;
+	}
+
+	int get_melee() const
+	{
+		return this->melee;
 	}
 
 	int get_range() const
@@ -106,7 +106,7 @@ public:
 
 	int get_score() const
 	{
-		return this->get_melee() + this->get_firepower() + this->get_range() + this->get_defense() + this->get_movement();
+		return this->get_firepower() + this->get_melee() + this->get_range() + this->get_defense() + this->get_movement();
 	}
 
 signals:
@@ -117,8 +117,8 @@ private:
 	metternich::culture *culture = nullptr;
 	metternich::cultural_group *cultural_group = nullptr;
 	metternich::icon *icon = nullptr;
-	int melee = 0;
 	int firepower = 0;
+	int melee = 0;
 	int range = 0;
 	int defense = 0;
 	int movement = 0;
