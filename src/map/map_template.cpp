bc2f59e2fc98ec1c0318bd94279fc33f823694f2
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-02-15 08:31:18 +0100

    Allow applying sites with no site type, as they may have history settings ruins there

--- src/map/map_template.cpp
+++ src/map/map_template.cpp
@@ -526,10 +526,6 @@ void map_template::apply_provinces() const
 
 	//apply tile sites
 	for (const auto &[tile_pos, site] : this->sites_by_position) {
-		if (site->get_type() == site_type::none) {
-			continue;
-		}
-
 		map->set_tile_site(tile_pos, site);
 	}
 }
