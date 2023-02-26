b3ed41c3ee9e0f56a4e46755305e1a8367d52322
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-02-26 21:15:19 +0100

    Enhanced the text processing

--- src/script/text_processor.cpp
+++ src/script/text_processor.cpp
@@ -4,6 +4,8 @@
 
 #include "map/province.h"
 #include "map/province_game_data.h"
+#include "map/site.h"
+#include "map/site_game_data.h"
 #include "util/assert_util.h"
 #include "util/queue_util.h"
 #include "util/string_util.h"
@@ -33,11 +35,9 @@ std::string text_processor::process_tokens(std::queue<std::string> &&tokens, con
 	std::string str;
 
 	if (front_subtoken == "root") {
-		if (std::holds_alternative<const province *>(this->context.root_scope)) {
-			str = this->process_province_tokens(std::get<const province *>(this->context.root_scope), tokens);
-		} else {
-			assert_throw(false);
-		}
+		str = this->process_scope_variant_tokens(this->context.root_scope, tokens);
+	} else if (front_subtoken == "source") {
+		str = this->process_scope_variant_tokens(this->context.source_scope, tokens);
 	} else {
 		throw std::runtime_error("Failed to process token \"" + token + "\".");
 	}
@@ -47,6 +47,19 @@ std::string text_processor::process_tokens(std::queue<std::string> &&tokens, con
 	}
 
 	return str;
+}
+
+std::string text_processor::process_scope_variant_tokens(const read_only_context::scope_variant_type &scope_variant, std::queue<std::string> &tokens) const
+{
+	if (std::holds_alternative<const province *>(scope_variant)) {
+		return this->process_province_tokens(std::get<const province *>(scope_variant), tokens);
+	} else if (std::holds_alternative<const site *>(scope_variant)) {
+		return this->process_site_tokens(std::get<const site *>(scope_variant), tokens);
+	} else {
+		assert_throw(false);
+	}
+
+	return std::string();
 }
 
 std::string text_processor::process_province_tokens(const province *province, std::queue<std::string> &tokens) const
@@ -73,6 +86,35 @@ std::string text_processor::process_province_tokens(const province *province, st
 		return province->get_game_data()->get_current_cultural_name();
 	} else {
 		return this->process_named_data_entry_token(province, front_subtoken);
+	}
+}
+
+std::string text_processor::process_site_tokens(const site *site, std::queue<std::string> &tokens) const
+{
+	if (site == nullptr) {
+		throw std::runtime_error("No site provided when processing site tokens.");
+	}
+
+	if (tokens.empty()) {
+		throw std::runtime_error("No tokens provided when processing site tokens.");
+	}
+
+	const std::string token = queue::take(tokens);
+
+	std::queue<std::string> subtokens = string::split_to_queue(token, ':');
+
+	if (subtokens.size() > 2) {
+		throw std::runtime_error("There can only be at most 2 subtokens.");
+	}
+
+	const std::string front_subtoken = queue::take(subtokens);
+
+	if (front_subtoken == "name") {
+		return site->get_game_data()->get_current_cultural_name();
+	} else if (front_subtoken == "location") {
+		return this->process_province_tokens(site->get_game_data()->get_province(), tokens);
+	} else {
+		return this->process_named_data_entry_token(site, front_subtoken);
 	}
 }
 
--- src/script/text_processor.h
+++ src/script/text_processor.h
@@ -13,7 +13,9 @@ public:
 	}
 
 	virtual std::string process_tokens(std::queue<std::string> &&tokens, const bool process_in_game_data, bool &processed) const override;
+	std::string process_scope_variant_tokens(const read_only_context::scope_variant_type &scope_variant, std::queue<std::string> &tokens) const;
 	std::string process_province_tokens(const province *province, std::queue<std::string> &tokens) const;
+	std::string process_site_tokens(const site *site, std::queue<std::string> &tokens) const;
 
 private:
 	const read_only_context context;
