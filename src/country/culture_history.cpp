bbb657f2dd436e19064d0414ce96be5976d26170
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-04-16 21:33:50 +0200

    Ignore provinces which are not on the map when applying exploration history

--- src/country/culture_history.cpp
+++ src/country/culture_history.cpp
@@ -4,6 +4,8 @@
 
 #include "country/country.h"
 #include "country/country_game_data.h"
+#include "map/province.h"
+#include "map/province_game_data.h"
 #include "map/region.h"
 
 namespace metternich {
@@ -17,11 +19,19 @@ void culture_history::apply_to_country(const country *country) const
 	}
 
 	for (const province *province : this->get_explored_provinces()) {
+		if (!province->get_game_data()->is_on_map()) {
+			continue;
+		}
+
 		country_game_data->explore_province(province);
 	}
 
 	for (const region *region : this->get_explored_regions()) {
 		for (const province *province : region->get_provinces()) {
+			if (!province->get_game_data()->is_on_map()) {
+				continue;
+			}
+
 			country_game_data->explore_province(province);
 		}
 	}
