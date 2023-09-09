72940329b9d224491379ab71fb69f1c8028befe3
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-09 13:13:12 +0200

    Allow resource sites to be character home settlements for now

--- src/character/character.cpp
+++ src/character/character.cpp
@@ -13,6 +13,7 @@
 #include "country/religion.h"
 #include "map/province.h"
 #include "map/site.h"
+#include "map/site_type.h"
 #include "script/condition/and_condition.h"
 #include "script/effect/effect_list.h"
 #include "script/modifier.h"
@@ -220,8 +221,8 @@ void character::check() const
 
 	if (this->get_home_settlement() == nullptr) {
 		throw std::runtime_error(std::format("Character \"{}\" has no home settlement.", this->get_identifier()));
-	} else if (!this->get_home_settlement()->is_settlement()) {
-		throw std::runtime_error(std::format("Character \"{}\" has \"{}\" set as their home settlement, but it is not a settlement.", this->get_identifier(), this->get_home_settlement()->get_identifier()));
+	} else if (!this->get_home_settlement()->is_settlement() && this->get_home_settlement()->get_type() != site_type::resource) {
+		throw std::runtime_error(std::format("Character \"{}\" has \"{}\" set as their home settlement, but it is neither a settlement nor a resource.", this->get_identifier(), this->get_home_settlement()->get_identifier()));
 	}
 
 	if (this->get_gender() == gender::none) {
