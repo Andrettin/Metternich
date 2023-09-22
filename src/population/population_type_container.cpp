e5421bd6744ccb4d27ef9e87bb2887365fe67483
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-22 20:20:30 +0200

    Update to the population type comparison function

--- src/population/population_type_container.cpp
+++ src/population/population_type_container.cpp
@@ -2,6 +2,7 @@
 
 #include "population/population_type_container.h"
 
+#include "economy/commodity.h"
 #include "population/population_type.h"
 
 namespace metternich {
@@ -10,6 +11,25 @@ bool population_type_compare::operator()(const population_type *lhs, const popul
 {
 	if ((lhs == nullptr || rhs == nullptr) && lhs != rhs) {
 		return lhs == nullptr;
+	}
+
+	const commodity *lhs_output_commodity = lhs->get_output_commodity();
+	const commodity *rhs_output_commodity = rhs->get_output_commodity();
+
+	if (lhs_output_commodity != rhs_output_commodity) {
+		if (lhs_output_commodity == nullptr || rhs_output_commodity == nullptr) {
+			return lhs_output_commodity == nullptr;
+		}
+
+		if (lhs_output_commodity->is_storable() != rhs_output_commodity->is_storable()) {
+			return lhs_output_commodity->is_storable();
+		}
+
+		return lhs_output_commodity->get_identifier() < rhs_output_commodity->get_identifier();
+	}
+
+	if (lhs->get_output_value() != rhs->get_output_value()) {
+		return lhs->get_output_value() < rhs->get_output_value();
 	}
 
 	return lhs->get_identifier() < rhs->get_identifier();
