b6d4e3deb3cc29d86bc50d68d52bae1f6d4bc31a
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-24 18:18:35 +0200

    The religious_group condition can now refer to a scope

--- src/script/condition/religious_group_condition.h
+++ src/script/condition/religious_group_condition.h
@@ -3,6 +3,8 @@
 #include "country/religion.h"
 #include "country/religious_group.h"
 #include "script/condition/condition.h"
+#include "script/target_variant.h"
+#include "util/assert_util.h"
 
 namespace metternich {
 
@@ -13,7 +15,11 @@ public:
 	explicit religious_group_condition(const std::string &value, const gsml_operator condition_operator)
 		: condition<scope_type>(condition_operator)
 	{
-		this->religious_group = religious_group::get(value);
+		if (enum_converter<special_target_type>::has_value(value)) {
+			this->religious_group_target = enum_converter<special_target_type>::to_enum(value);
+		} else {
+			this->religious_group_target = religious_group::get(value);
+		}
 	}
 
 	virtual const std::string &get_class_identifier() const override
@@ -39,18 +45,58 @@ public:
 			religious_group = religion->get_group();
 		}
 
-		return religious_group == this->religious_group;
+		return religious_group == this->get_religious_group(ctx);
 	}
 
 	virtual std::string get_assignment_string(const size_t indent) const override
 	{
 		Q_UNUSED(indent);
 
-		return this->religious_group->get_name() + " religious group";
+		if (std::holds_alternative<const religious_group *>(this->religious_group_target)) {
+			return std::get<const religious_group *>(this->religious_group_target)->get_name() + " religious group";
+		} else if (std::holds_alternative<special_target_type>(this->religious_group_target)) {
+			const special_target_type target_type = std::get<special_target_type>(this->religious_group_target);
+			return string::capitalized(enum_converter<special_target_type>::to_string(target_type)) + " scope religious group";
+		} else {
+			assert_throw(false);
+			return std::string();
+		}
+	}
+
+	const religious_group *get_religious_group(const read_only_context &ctx) const
+	{
+		if (std::holds_alternative<const religious_group *>(this->religious_group_target)) {
+			return std::get<const religious_group *>(this->religious_group_target);
+		} else if (std::holds_alternative<special_target_type>(this->religious_group_target)) {
+			const special_target_type target_type = std::get<special_target_type>(this->religious_group_target);
+			const read_only_context::scope_variant_type &target_scope_variant = ctx.get_special_target_scope_variant(target_type);
+
+			const religion *religion = std::visit([](auto &&target_scope) -> const metternich::religion * {
+				using target_scope_type = std::remove_const_t<std::remove_pointer_t<std::decay_t<decltype(target_scope)>>>;
+
+				if constexpr (std::is_same_v<target_scope_type, std::monostate>) {
+					assert_throw(false);
+					return nullptr;
+				} else if constexpr (std::is_same_v<target_scope_type, country> || std::is_same_v<target_scope_type, province> || std::is_same_v<target_scope_type, site>) {
+					return target_scope->get_game_data()->get_religion();
+				} else {
+					return target_scope->get_religion();
+				}
+			}, target_scope_variant);
+
+			if (religion != nullptr) {
+				return religion->get_group();
+			}
+
+			return nullptr;
+		}
+
+		assert_throw(false);
+		return nullptr;
 	}
 
 private:
-	const metternich::religious_group *religious_group = nullptr;
+	target_variant<const religious_group> religious_group_target;
 };
 
 }
