fe4672f1926a2d5ca92cd8d8e205fa484f96e53b
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-10-01 08:45:25 +0200

    Added support for commodities having a base price

--- src/economy/commodity.cpp
+++ src/economy/commodity.cpp
@@ -33,8 +33,16 @@ void commodity::check() const
 		}
 
 		if (this->is_storable()) {
-			throw std::runtime_error(std::format("Commodity \"{}\" is local and storable, which is not supported.", this->get_identifier()));
+			throw std::runtime_error(std::format("Commodity \"{}\" is both local and storable, which is not supported.", this->get_identifier()));
 		}
+	}
+
+	if (!this->is_abstract() && this->is_storable() && this->get_base_price() == 0 && this->get_wealth_value() == 0) {
+		throw std::runtime_error(std::format("Non-abstract storable commodity \"{}\" has neither a base price nor a wealth value.", this->get_identifier()));
+	}
+
+	if (this->get_base_price() != 0 && this->get_wealth_value() != 0) {
+		throw std::runtime_error(std::format("Commodity \"{}\" has both a base price and a wealth value.", this->get_identifier()));
 	}
 }
 
--- src/economy/commodity.h
+++ src/economy/commodity.h
@@ -17,12 +17,13 @@ class commodity final : public named_data_entry, public data_type<commodity>
 
 	Q_PROPERTY(metternich::icon* icon MEMBER icon NOTIFY changed)
 	Q_PROPERTY(metternich::food_type food_type MEMBER food_type READ get_food_type NOTIFY changed)
-	Q_PROPERTY(int wealth_value MEMBER wealth_value READ get_wealth_value NOTIFY changed)
 	Q_PROPERTY(bool abstract MEMBER abstract READ is_abstract NOTIFY changed)
 	Q_PROPERTY(bool storable MEMBER storable READ is_storable NOTIFY changed)
 	Q_PROPERTY(bool local MEMBER local READ is_local NOTIFY changed)
 	Q_PROPERTY(bool negative_allowed MEMBER negative_allowed READ is_negative_allowed NOTIFY changed)
 	Q_PROPERTY(bool labor MEMBER labor READ is_labor NOTIFY changed)
+	Q_PROPERTY(int wealth_value MEMBER wealth_value READ get_wealth_value NOTIFY changed)
+	Q_PROPERTY(int base_price MEMBER base_price READ get_base_price NOTIFY changed)
 	Q_PROPERTY(metternich::technology* required_technology MEMBER required_technology NOTIFY changed)
 
 public:
@@ -45,16 +46,6 @@ public:
 	metternich::food_type get_food_type() const
 	{
 		return this->food_type;
-	}
-
-	int get_wealth_value() const
-	{
-		return this->wealth_value;
-	}
-
-	bool is_convertible_to_wealth() const
-	{
-		return this->get_wealth_value() != 0;
 	}
 
 	bool is_abstract() const
@@ -82,6 +73,21 @@ public:
 		return this->labor;
 	}
 
+	int get_wealth_value() const
+	{
+		return this->wealth_value;
+	}
+
+	bool is_convertible_to_wealth() const
+	{
+		return this->get_wealth_value() != 0;
+	}
+
+	int get_base_price() const
+	{
+		return this->base_price;
+	}
+
 	const technology *get_required_technology() const
 	{
 		return this->required_technology;
@@ -93,12 +99,13 @@ signals:
 private:
 	metternich::icon *icon = nullptr;
 	metternich::food_type food_type;
-	int wealth_value = 0;
 	bool abstract = false;
 	bool storable = true;
 	bool local = false;
 	bool negative_allowed = false;
 	bool labor = false;
+	int wealth_value = 0;
+	int base_price = 0;
 	technology *required_technology = nullptr;
 };
 
