287b4bd9fd5ab115bd285de83247a78e94c16cb9
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-10-04 18:52:59 +0200

    Added support for defining policies in the database

---
+++ src/country/policy.cpp
@@ -0,0 +1,80 @@
+#include "metternich.h"
+
+#include "country/policy.h"
+
+#include "script/modifier.h"
+#include "util/assert_util.h"
+
+namespace metternich {
+
+void policy::process_gsml_scope(const gsml_data &scope)
+{
+	const std::string &tag = scope.get_tag();
+
+	if (tag == "modifier") {
+		auto modifier = std::make_unique<metternich::modifier<const country>>();
+		database::process_gsml_data(modifier, scope);
+		this->modifier = std::move(modifier);
+	} else if (tag == "left_modifier") {
+		auto modifier = std::make_unique<metternich::modifier<const country>>();
+		database::process_gsml_data(modifier, scope);
+		this->left_modifier = std::move(modifier);
+	} else if (tag == "right_modifier") {
+		auto modifier = std::make_unique<metternich::modifier<const country>>();
+		database::process_gsml_data(modifier, scope);
+		this->right_modifier = std::move(modifier);
+	} else {
+		data_entry::process_gsml_scope(scope);
+	}
+}
+
+void policy::check() const
+{
+	if (this->get_modifier() == nullptr) {
+		throw std::runtime_error(std::format("Policy \"{}\" has no modifier.", this->get_identifier()));
+	}
+}
+
+void policy::apply_modifier(const country *country, const int level, const int multiplier)
+{
+	if (this->get_modifier() != nullptr) {
+		this->get_modifier()->apply(country, level * multiplier);
+	}
+
+	if (this->get_left_modifier() != nullptr && level < 0) {
+		this->get_left_modifier()->apply(country, std::abs(level) * multiplier);
+	}
+
+	if (this->get_right_modifier() != nullptr && level > 0) {
+		this->get_right_modifier()->apply(country, level * multiplier);
+	}
+}
+
+QString policy::get_modifier_string(const country *country, const int level) const
+{
+	std::string str;
+
+	if (this->get_modifier() != nullptr) {
+		str += this->get_modifier()->get_string(country, level);
+	}
+
+	if (this->get_left_modifier() != nullptr && level < 0) {
+		if (!str.empty()) {
+			str += "\n";
+		}
+
+		str += this->get_left_modifier()->get_string(country, std::abs(level));
+	}
+
+	if (this->get_right_modifier() != nullptr && level > 0) {
+		if (!str.empty()) {
+			str += "\n";
+		}
+
+		str += this->get_right_modifier()->get_string(country, level);
+	}
+
+	return QString::fromStdString(str);
+}
+
+}
---
+++ src/country/policy.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "database/data_entry.h"
+#include "database/data_type.h"
+
+namespace metternich {
+
+class country;
+
+template <typename scope_type>
+class modifier;
+
+class policy final : public data_entry, public data_type<policy>
+{
+	Q_OBJECT
+
+	Q_PROPERTY(QString left_name READ get_left_name_qstring NOTIFY changed)
+	Q_PROPERTY(QString right_name READ get_right_name_qstring NOTIFY changed)
+
+public:
+	static constexpr const char class_identifier[] = "policy";
+	static constexpr const char property_class_identifier[] = "metternich::policy*";
+	static constexpr const char database_folder[] = "policies";
+
+	static constexpr int min_level = -5;
+	static constexpr int max_level = 5;
+
+	explicit policy(const std::string &identifier) : data_entry(identifier)
+	{
+	}
+
+	virtual void process_gsml_scope(const gsml_data &scope) override;
+	virtual void check() const override;
+
+	const std::string &get_left_name() const
+	{
+		return this->left_name;
+	}
+
+	Q_INVOKABLE void set_left_name(const std::string &name)
+	{
+		this->left_name = name;
+	}
+
+	QString get_left_name_qstring() const
+	{
+		return QString::fromStdString(this->get_left_name());
+	}
+
+	const std::string &get_right_name() const
+	{
+		return this->right_name;
+	}
+
+	Q_INVOKABLE void set_right_name(const std::string &name)
+	{
+		this->right_name = name;
+	}
+
+	QString get_right_name_qstring() const
+	{
+		return QString::fromStdString(this->get_right_name());
+	}
+
+	const metternich::modifier<const country> *get_modifier() const
+	{
+		return this->modifier.get();
+	}
+
+	const metternich::modifier<const country> *get_left_modifier() const
+	{
+		return this->left_modifier.get();
+	}
+
+	const metternich::modifier<const country> *get_right_modifier() const
+	{
+		return this->right_modifier.get();
+	}
+
+	void apply_modifier(const country *country, const int level, const int multiplier);
+	Q_INVOKABLE QString get_modifier_string(const metternich::country *country, const int level) const;
+
+signals:
+	void changed();
+
+private:
+	std::string left_name;
+	std::string right_name;
+	std::unique_ptr<const metternich::modifier<const country>> modifier;
+	std::unique_ptr<const metternich::modifier<const country>> left_modifier;
+	std::unique_ptr<const metternich::modifier<const country>> right_modifier;
+};
+
+}
