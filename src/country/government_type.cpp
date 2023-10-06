efb9ddbe2d6da3292b1baa0f96f2b86811854642
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-10-06 19:53:41 +0200

    Fix to government type title names

--- src/country/government_type.cpp
+++ src/country/government_type.cpp
@@ -10,6 +10,42 @@
 
 namespace metternich {
 
+void government_type::process_title_names(title_name_map &title_names, const gsml_data &scope)
+{
+	scope.for_each_property([&](const gsml_property &property) {
+		const std::string &key = property.get_key();
+		const std::string &value = property.get_value();
+		const country_tier tier = enum_converter<country_tier>::to_enum(key);
+		title_names[tier] = value;
+	});
+}
+
+void government_type::process_ruler_title_names(ruler_title_name_map &ruler_title_names, const gsml_data &scope)
+{
+	scope.for_each_property([&](const gsml_property &property) {
+		const std::string &key = property.get_key();
+		const std::string &value = property.get_value();
+		const country_tier tier = enum_converter<country_tier>::to_enum(key);
+		ruler_title_names[tier][gender::none] = value;
+	});
+
+	scope.for_each_child([&](const gsml_data &child_scope) {
+		const country_tier tier = enum_converter<country_tier>::to_enum(child_scope.get_tag());
+
+		government_type::process_ruler_title_name_scope(ruler_title_names[tier], child_scope);
+	});
+}
+
+void government_type::process_ruler_title_name_scope(std::map<gender, std::string> &ruler_title_names, const gsml_data &scope)
+{
+	scope.for_each_property([&](const gsml_property &property) {
+		const std::string &key = property.get_key();
+		const std::string &value = property.get_value();
+		const gender gender = enum_converter<archimedes::gender>::to_enum(key);
+		ruler_title_names[gender] = value;
+	});
+}
+
 government_type::government_type(const std::string &identifier) : named_data_entry(identifier)
 {
 }
@@ -26,6 +62,10 @@ void government_type::process_gsml_scope(const gsml_data &scope)
 		auto modifier = std::make_unique<metternich::modifier<const country>>();
 		database::process_gsml_data(modifier, scope);
 		this->modifier = std::move(modifier);
+	} else if (tag == "title_names") {
+		government_type::process_title_names(this->title_names, scope);
+	} else if (tag == "ruler_title_names") {
+		government_type::process_ruler_title_names(this->ruler_title_names, scope);
 	} else {
 		data_entry::process_gsml_scope(scope);
 	}
