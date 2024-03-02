7677422574945caf2356d17db7dc0dbf5ea1e650
Andrettin <6322423+Andrettin@users.noreply.github.com>
2024-03-02 18:42:32 +0100

    Added land_transporter transporter category

--- src/unit/transporter_category.cpp
+++ src/unit/transporter_category.cpp
@@ -11,6 +11,7 @@ const std::string enum_converter<metternich::transporter_category>::property_cla
 
 template <>
 const std::map<std::string, metternich::transporter_category> enum_converter<metternich::transporter_category>::string_to_enum_map = {
+	{ "land_transporter", metternich::transporter_category::land_transporter },
 	{ "small_merchant_ship", metternich::transporter_category::small_merchant_ship },
 	{ "large_merchant_ship", metternich::transporter_category::large_merchant_ship }
 };
--- src/unit/transporter_category.h
+++ src/unit/transporter_category.h
@@ -6,6 +6,7 @@ namespace metternich {
 
 enum class transporter_category {
 	none,
+	land_transporter,
 	small_merchant_ship,
 	large_merchant_ship
 };
