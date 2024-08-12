8496c9906658b2c03cadc5deb9f2d5160d4346bb
Andrettin <6322423+Andrettin@users.noreply.github.com>
2024-08-12 20:31:56 +0200

    Added Spear Infantry, Blade Infantry and Mace Infantry military unit categories

--- src/unit/military_unit_category.h
+++ src/unit/military_unit_category.h
@@ -13,6 +13,9 @@ enum class military_unit_category {
 	flying_beasts,
 	colossal_flying_beasts,
 	militia,
+	mace_infantry,
+	spear_infantry,
+	blade_infantry,
 	light_infantry,
 	regular_infantry,
 	heavy_infantry,
@@ -53,6 +56,12 @@ inline std::string_view get_military_unit_category_name(const military_unit_cate
 			return "Colossal Flying Beasts";
 		case military_unit_category::militia:
 			return "Militia";
+		case military_unit_category::mace_infantry:
+			return "Mace Infantry";
+		case military_unit_category::spear_infantry:
+			return "Spear Infantry";
+		case military_unit_category::blade_infantry:
+			return "Blade Infantry";
 		case military_unit_category::light_infantry:
 			return "Light Infantry";
 		case military_unit_category::regular_infantry:
