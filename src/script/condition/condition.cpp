b117bc35bb7b62671651f20b0695af5418b6c55d
Andrettin <6322423+Andrettin@users.noreply.github.com>
2024-10-19 13:26:55 +0200

    Update for the MaskedMouseArea and some of the condition code having been moved to Archimedes

--- src/script/condition/condition.cpp
+++ src/script/condition/condition.cpp
@@ -425,16 +425,6 @@ std::unique_ptr<const condition<scope_type>> condition<scope_type>::from_gsml_sc
 }
 
 template <typename scope_type>
-std::string condition<scope_type>::get_object_highlighted_name(const named_data_entry *object, const std::string &name_string)
-{
-	if (!name_string.empty()) {
-		return string::highlight(name_string);
-	} else {
-		return string::highlight(object->get_name());
-	}
-}
-
-template <typename scope_type>
 const country *condition<scope_type>::get_scope_country(const scope_type *scope)
 {
 	if constexpr (std::is_same_v<scope_type, character>) {
@@ -457,70 +447,6 @@ const province *condition<scope_type>::get_scope_province(const scope_type *scop
 		return scope;
 	} else if constexpr (std::is_same_v<scope_type, site>) {
 		return scope->get_game_data()->get_province();
-	}
-}
-
-template <typename scope_type>
-condition<scope_type>::condition(const gsml_operator condition_operator) : condition_operator(condition_operator)
-{
-}
-
-template <typename scope_type>
-void condition<scope_type>::process_gsml_property(const gsml_property &property)
-{
-	throw std::runtime_error("Invalid \"" + this->get_class_identifier() + "\" condition property: \"" + property.get_key() + "\".");
-}
-
-template <typename scope_type>
-void condition<scope_type>::process_gsml_scope(const gsml_data &scope)
-{
-	throw std::runtime_error("Invalid \"" + this->get_class_identifier() + "\" condition scope: \"" + scope.get_tag() + "\".");
-}
-
-
-template <typename scope_type>
-bool condition<scope_type>::check(const scope_type *scope, const read_only_context &ctx) const
-{
-	switch (this->condition_operator) {
-		case gsml_operator::assignment:
-			return this->check_assignment(scope, ctx);
-		case gsml_operator::equality:
-			return this->check_equality(scope);
-		case gsml_operator::inequality:
-			return this->check_inequality(scope);
-		case gsml_operator::less_than:
-			return this->check_less_than(scope);
-		case gsml_operator::less_than_or_equality:
-			return this->check_less_than_or_equality(scope);
-		case gsml_operator::greater_than:
-			return this->check_greater_than(scope);
-		case gsml_operator::greater_than_or_equality:
-			return this->check_greater_than_or_equality(scope);
-		default:
-			throw std::runtime_error("Invalid condition operator: \"" + std::to_string(static_cast<int>(this->condition_operator)) + "\".");
-	}
-}
-
-template <typename scope_type>
-std::string condition<scope_type>::get_string(const size_t indent) const
-{
-	switch (this->condition_operator) {
-		case gsml_operator::assignment:
-			return this->get_assignment_string(indent);
-		case gsml_operator::equality:
-			return this->get_equality_string();
-		case gsml_operator::inequality:
-			return this->get_inequality_string();
-		case gsml_operator::less_than:
-			return this->get_less_than_string();
-		case gsml_operator::less_than_or_equality:
-			return this->get_less_than_or_equality_string();
-		case gsml_operator::greater_than:
-			return this->get_greater_than_string();
-		case gsml_operator::greater_than_or_equality:
-			return this->get_greater_than_or_equality_string();
-		default:
-			throw std::runtime_error("Invalid condition operator: \"" + std::to_string(static_cast<int>(this->condition_operator)) + "\".");
 	}
 }
 
--- src/script/condition/condition.h
+++ src/script/condition/condition.h
@@ -1,13 +1,7 @@
 #pragma once
 
+#include "script/condition_base.h"
 #include "script/context.h"
-
-namespace archimedes {
-	class gsml_data;
-	class gsml_property;
-	class named_data_entry;
-	enum class gsml_operator;
-}
 
 namespace metternich {
 
@@ -16,7 +10,7 @@ class province;
 struct read_only_context;
 
 template <typename scope_type>
-class condition
+class condition : public condition_base<scope_type, read_only_context>
 {
 public:
 	static std::unique_ptr<const condition> from_gsml_property(const gsml_property &property);
@@ -51,112 +45,16 @@ public:
 		return conditions_string;
 	}
 
-	//get the string for the object of a condition, e.g. the unit type for a unit type condition
-	template <typename T>
-	static std::string get_object_string(const T *object, const std::string &name_string = "")
-	{
-		return condition<scope_type>::get_object_highlighted_name(object, name_string);
-	}
-
-	static std::string get_object_highlighted_name(const named_data_entry *object, const std::string &name_string);
-
 	static const country *get_scope_country(const scope_type *scope);
 	static const province *get_scope_province(const scope_type *scope);
 
-	explicit condition(const gsml_operator condition_operator);
+	explicit condition(const gsml_operator condition_operator) : condition_base<scope_type, read_only_context>(condition_operator)
+	{
+	}
 
 	virtual ~condition()
 	{
 	}
-
-	virtual const std::string &get_class_identifier() const = 0;
-
-	virtual void process_gsml_property(const gsml_property &property);
-	virtual void process_gsml_scope(const gsml_data &scope);
-
-	virtual void check_validity() const
-	{
-	}
-
-	bool check(const scope_type *scope, const read_only_context &ctx) const;
-	virtual bool check_assignment(const scope_type *scope, const read_only_context &ctx) const = 0;
-
-	virtual bool check_equality(const scope_type *scope) const
-	{
-		Q_UNUSED(scope);
-
-		throw std::runtime_error("The equality operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual bool check_inequality(const scope_type *scope) const
-	{
-		return !this->check_equality(scope);
-	}
-
-	virtual bool check_less_than(const scope_type *scope) const
-	{
-		Q_UNUSED(scope);
-
-		throw std::runtime_error("The less than operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual bool check_less_than_or_equality(const scope_type *scope) const
-	{
-		return this->check_equality(scope) || this->check_less_than(scope);
-	}
-
-	virtual bool check_greater_than(const scope_type *scope) const
-	{
-		Q_UNUSED(scope);
-
-		throw std::runtime_error("The greater than operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual bool check_greater_than_or_equality(const scope_type *scope) const
-	{
-		return this->check_equality(scope) || this->check_greater_than(scope);
-	}
-
-	std::string get_string(const size_t indent) const;
-	virtual std::string get_assignment_string(const size_t indent) const = 0;
-
-	virtual std::string get_equality_string() const
-	{
-		throw std::runtime_error("The equality operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual std::string get_inequality_string() const
-	{
-		throw std::runtime_error("The inequality operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual std::string get_less_than_string() const
-	{
-		throw std::runtime_error("The less than operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual std::string get_less_than_or_equality_string() const
-	{
-		throw std::runtime_error("The less than or equality operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual std::string get_greater_than_string() const
-	{
-		throw std::runtime_error("The greater than operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual std::string get_greater_than_or_equality_string() const
-	{
-		throw std::runtime_error("The greater than or equality operator is not supported for \"" + this->get_class_identifier() + "\" conditions.");
-	}
-
-	virtual bool is_hidden() const
-	{
-		return false;
-	}
-
-private:
-	gsml_operator condition_operator;
 };
 
 }
