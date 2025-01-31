0e0ff576feca3ccd6a7a6fba8064ee6e8798a371
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-01-31 08:46:46 +0100

    Minor update to the saved_scope effect source code

--- src/script/effect/saved_scope_effect.h
+++ src/script/effect/saved_scope_effect.h
@@ -19,6 +19,15 @@ public:
 		return class_identifier;
 	}
 
+	virtual void process_gsml_property(const gsml_property &property) override
+	{
+		if (property.get_key() == "scope") {
+			this->scope_name = property.get_value();
+		} else {
+			scope_effect<upper_scope_type, scope_type>::process_gsml_property(property);
+		}
+	}
+
 	virtual const scope_type *get_scope(const upper_scope_type *upper_scope, const read_only_context &ctx) const
 	{
 		Q_UNUSED(upper_scope);
@@ -31,15 +40,6 @@ public:
 		Q_UNUSED(upper_scope);
 
 		return ctx.get_saved_scope<scope_type>(this->scope_name);
-	}
-
-	virtual void process_gsml_property(const gsml_property &property) override
-	{
-		if (property.get_key() == "scope") {
-			this->scope_name = property.get_value();
-		} else {
-			scope_effect<upper_scope_type, scope_type>::process_gsml_property(property);
-		}
 	}
 
 private:
