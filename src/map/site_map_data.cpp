3d11eee80a0b6807792506cd65815c7d106f4545
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-06-30 00:08:31 +0200

    Update to "map" source files based on changes from Kobold

--- src/map/site_map_data.cpp
+++ src/map/site_map_data.cpp
@@ -27,6 +27,8 @@ site_map_data::site_map_data(const metternich::site *site)
 
 void site_map_data::set_tile_pos(const QPoint &tile_pos)
 {
+	assert_throw(this->get_tile_pos() == QPoint(-1, -1));
+
 	if (tile_pos == this->get_tile_pos()) {
 		return;
 	}
