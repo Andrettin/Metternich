f381f30d3568b501fdccd5a3d068a480170e00a3
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-06-30 22:12:52 +0200

    Improved dynasty validity checking

--- src/character/dynasty.cpp
+++ src/character/dynasty.cpp
@@ -2,14 +2,12 @@
 
 #include "character/dynasty.h"
 
-#include "util/assert_util.h"
-
 namespace metternich {
 
 void dynasty::check() const
 {
 	if (this->get_culture() == nullptr) {
-		throw std::runtime_error("Dynasty \"" + this->get_identifier() + "\" has no culture.");
+		throw std::runtime_error(std::format("Dynasty \"{}\" has no culture.", this->get_identifier()));
 	}
 }
 
