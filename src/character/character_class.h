5db4a3e86681bb8b712cbcb82d986e21cf7afa6a
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-09-05 22:53:35 +0200

    Added support for character classes having damage dice

--- src/character/character_class.h
+++ src/character/character_class.h
@@ -31,6 +31,7 @@ class character_class final : public named_data_entry, public data_type<characte
 	Q_PROPERTY(const metternich::civilian_unit_class* civilian_unit_class MEMBER civilian_unit_class READ get_civilian_unit_class NOTIFY changed)
 	Q_PROPERTY(int max_level MEMBER max_level READ get_max_level NOTIFY changed)
 	Q_PROPERTY(archimedes::dice hit_dice MEMBER hit_dice READ get_hit_dice NOTIFY changed)
+	Q_PROPERTY(archimedes::dice damage_dice MEMBER damage_dice READ get_damage_dice NOTIFY changed)
 	Q_PROPERTY(metternich::starting_age_category starting_age_category MEMBER starting_age_category READ get_starting_age_category NOTIFY changed)
 	Q_PROPERTY(metternich::technology* required_technology MEMBER required_technology NOTIFY changed)
 	Q_PROPERTY(metternich::technology* obsolescence_technology MEMBER obsolescence_technology NOTIFY changed)
@@ -70,6 +71,11 @@ public:
 		return this->hit_dice;
 	}
 
+	const dice &get_damage_dice() const
+	{
+		return this->damage_dice;
+	}
+
 	metternich::starting_age_category get_starting_age_category() const
 	{
 		return this->starting_age_category;
@@ -94,6 +100,7 @@ private:
 	const metternich::civilian_unit_class *civilian_unit_class = nullptr;
 	int max_level = 0;
 	dice hit_dice;
+	dice damage_dice;
 	metternich::starting_age_category starting_age_category{};
 	technology *required_technology = nullptr;
 	technology *obsolescence_technology = nullptr;
