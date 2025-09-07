ae63746be635a12b7bc37e03f8091b9cc0547ca5
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-09-07 21:19:03 +0200

    Added support for character classes to have an armor class

--- src/character/character_class.h
+++ src/character/character_class.h
@@ -32,6 +32,7 @@ class character_class final : public named_data_entry, public data_type<characte
 	Q_PROPERTY(int max_level MEMBER max_level READ get_max_level NOTIFY changed)
 	Q_PROPERTY(archimedes::dice hit_dice MEMBER hit_dice READ get_hit_dice NOTIFY changed)
 	Q_PROPERTY(archimedes::dice damage_dice MEMBER damage_dice READ get_damage_dice NOTIFY changed)
+	Q_PROPERTY(int armor_class MEMBER armor_class READ get_armor_class NOTIFY changed)
 	Q_PROPERTY(metternich::starting_age_category starting_age_category MEMBER starting_age_category READ get_starting_age_category NOTIFY changed)
 	Q_PROPERTY(metternich::technology* required_technology MEMBER required_technology NOTIFY changed)
 	Q_PROPERTY(metternich::technology* obsolescence_technology MEMBER obsolescence_technology NOTIFY changed)
@@ -76,6 +77,11 @@ public:
 		return this->damage_dice;
 	}
 
+	int get_armor_class() const
+	{
+		return this->armor_class;
+	}
+
 	metternich::starting_age_category get_starting_age_category() const
 	{
 		return this->starting_age_category;
@@ -101,6 +107,7 @@ private:
 	int max_level = 0;
 	dice hit_dice;
 	dice damage_dice;
+	int armor_class = 0;
 	metternich::starting_age_category starting_age_category{};
 	technology *required_technology = nullptr;
 	technology *obsolescence_technology = nullptr;
