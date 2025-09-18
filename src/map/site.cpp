716e2584a91d2f4f86a140ceeb0c753650f90938
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-09-18 09:48:55 +0200

    Removed error message for when settlement sites have no resource

--- src/map/site.cpp
+++ src/map/site.cpp
@@ -97,10 +97,6 @@ void site::check() const
 	switch (this->get_type()) {
 		case site_type::settlement:
 		case site_type::habitable_world:
-			if (this->get_resource() == nullptr) {
-				log::log_error(std::format("Settlement site \"{}\" has no resource.", this->get_identifier()));
-			}
-
 			if (this->get_province() != nullptr) {
 				if (this->get_province()->get_provincial_capital() != this) {
 					log::log_error(std::format("Settlement site \"{}\" is not the provincial capital of its province.", this->get_identifier()));
