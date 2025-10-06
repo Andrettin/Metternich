6fac8373f26656279124a3444393982fc8121e1b
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-10-06 22:46:45 +0200

    Update to previous commit

---
+++ src/script/condition/domain_exists_condition.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "domain/domain.h"
+#include "game/game.h"
+#include "script/condition/condition.h"
+#include "util/vector_util.h"
+
+namespace metternich {
+
+template <typename scope_type>
+class domain_exists_condition final : public condition<scope_type>
+{
+public:
+	explicit domain_exists_condition(const std::string &value, const gsml_operator condition_operator)
+		: condition<scope_type>(condition_operator)
+	{
+		this->domain = domain::get(value);
+	}
+
+	virtual const std::string &get_class_identifier() const override
+	{
+		static const std::string class_identifier = "domain_exists";
+		return class_identifier;
+	}
+
+	virtual bool check_assignment(const scope_type *scope, const read_only_context &ctx) const override
+	{
+		Q_UNUSED(scope);
+		Q_UNUSED(ctx);
+
+		return vector::contains(game::get()->get_countries(), this->domain);
+	}
+
+	virtual std::string get_assignment_string(const size_t indent) const override
+	{
+		Q_UNUSED(indent);
+
+		return this->domain->get_game_data()->get_name() + " exists";
+	}
+
+private:
+	const metternich::domain *domain = nullptr;
+};
+
+}
