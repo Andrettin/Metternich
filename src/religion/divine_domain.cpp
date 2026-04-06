a08e0ec92111fb89bcb043f9d0cf9da01b05267a
Andrettin <6322423+Andrettin@users.noreply.github.com>
2026-04-06 12:17:58 +0200

    Added trace logging for the quantity for spells for each arcane school and divine domain

--- src/religion/divine_domain.cpp
+++ src/religion/divine_domain.cpp
@@ -11,6 +11,8 @@ void divine_domain::check() const
 	if (this->get_spells().empty()) {
 		log::log_error(std::format("Divine domain \"{}\" has no spells.", this->get_identifier()));
 	}
+
+	log_trace(std::format("Divine domain \"{}\" has {} spells.", this->get_identifier(), this->get_spells().size()));
 }
 
 }
--- src/spell/arcane_school.cpp
+++ src/spell/arcane_school.cpp
@@ -11,6 +11,8 @@ void arcane_school::check() const
 	if (this->get_spells().empty()) {
 		log::log_error(std::format("Arcane school \"{}\" has no spells.", this->get_identifier()));
 	}
+
+	log_trace(std::format("Arcane school \"{}\" has {} spells.", this->get_identifier(), this->get_spells().size()));
 }
 
 }
