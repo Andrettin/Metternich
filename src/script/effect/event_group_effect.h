8c7200f9a5d779226427fd0311d122551b83da1b
Andrettin <6322423+Andrettin@users.noreply.github.com>
2026-04-10 23:03:02 +0200

    Allow the event group effect to trigger any per-turn-pulse random event

--- src/script/effect/event_group_effect.h
+++ src/script/effect/event_group_effect.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "game/event_random_group.h"
+#include "game/event_trigger.h"
 #include "script/effect/effect.h"
 #include "util/assert_util.h"
 
@@ -12,7 +13,11 @@ class event_group_effect final : public effect<scope_type>
 public:
 	explicit event_group_effect(const std::string &value, const gsml_operator effect_operator) : effect<scope_type>(effect_operator)
 	{
-		this->event_group = event_random_group::get(value);
+		if (value == "random") {
+			this->random_event = true;
+		} else {
+			this->event_group = event_random_group::get(value);
+		}
 	}
 
 	virtual const std::string &get_class_identifier() const override
@@ -23,8 +28,8 @@ public:
 
 	virtual void check() const override
 	{
-		if (this->event_group == nullptr) {
-			throw std::runtime_error(std::format("\"{}\" effect has no event group set for it.", this->get_class_identifier()));
+		if (this->event_group == nullptr && !this->random_event) {
+			throw std::runtime_error(std::format("\"{}\" effect has no event group set for it, and it also does not trigger a random event.", this->get_class_identifier()));
 		}
 	}
 
@@ -34,7 +39,9 @@ public:
 		event_ctx.root_scope = scope;
 		event_ctx.source_scope = ctx.root_scope;
 
-		co_await scoped_event_base<scope_type>::check_random_events_for_scope(scope, event_ctx, this->event_group->get_events<scope_type>(), this->event_group->get_delay(game::get()->get_year()));
+		const auto &events = this->event_group != nullptr ? this->event_group->get_events<scope_type>() : scoped_event_base<scope_type>::get_trigger_random_events(event_trigger::per_turn_pulse);
+
+		co_await scoped_event_base<scope_type>::check_random_events_for_scope(scope, event_ctx, events, this->event_group != nullptr ? this->event_group->get_delay(game::get()->get_year()) : 0);
 	}
 
 	virtual std::string get_assignment_string(const scope_type *scope, const read_only_context &ctx, const size_t indent, const std::string &prefix) const override
@@ -44,10 +51,15 @@ public:
 		Q_UNUSED(indent);
 		Q_UNUSED(prefix);
 
+		if (this->random_event) {
+			return "Trigger a random event";
+		}
+
 		return std::format("Trigger {} {} event", string::get_indefinite_article(this->event_group->get_name()), this->event_group->get_name());
 	}
 
 private:
+	bool random_event = false;
 	const event_random_group *event_group = nullptr;
 };
 
