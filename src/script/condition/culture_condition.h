45f5c181f5ceb521db06c67520ce4b171909b96c
Andrettin <6322423+Andrettin@users.noreply.github.com>
2026-04-12 12:07:08 +0200

    Allow the culture condition to refer to the culture of a special target

--- src/script/condition/culture_condition.h
+++ src/script/condition/culture_condition.h
@@ -2,6 +2,7 @@
 
 #include "culture/culture.h"
 #include "script/condition/condition.h"
+#include "script/target_variant.h"
 
 namespace metternich {
 
@@ -12,7 +13,11 @@ public:
 	explicit culture_condition(const std::string &value, const gsml_operator condition_operator)
 		: condition<scope_type>(condition_operator)
 	{
-		this->culture = culture::get(value);
+		if (magic_enum::enum_contains<special_target_type>(value)) {
+			this->culture_target = magic_enum::enum_cast<special_target_type>(value).value();
+		} else {
+			this->culture_target = culture::get(value);
+		}
 	}
 
 	virtual const std::string &get_class_identifier() const override
@@ -33,18 +38,52 @@ public:
 			culture = scope->get_game_data()->get_culture();
 		}
 
-		return culture == this->culture;
+		return culture == this->get_culture(ctx);
 	}
 
 	virtual std::string get_assignment_string(const size_t indent) const override
 	{
 		Q_UNUSED(indent);
 
-		return string::highlight(this->culture->get_name()) + " culture";
+		if (std::holds_alternative<const culture *>(this->culture_target)) {
+			return std::get<const culture *>(this->culture_target)->get_name() + " culture";
+		} else if (std::holds_alternative<special_target_type>(this->culture_target)) {
+			const special_target_type target_type = std::get<special_target_type>(this->culture_target);
+			return string::capitalized(std::string(magic_enum::enum_name(target_type))) + " scope culture";
+		} else {
+			assert_throw(false);
+			return std::string();
+		}
+	}
+
+	const culture *get_culture(const read_only_context &ctx) const
+	{
+		if (std::holds_alternative<const culture *>(this->culture_target)) {
+			return std::get<const culture *>(this->culture_target);
+		} else if (std::holds_alternative<special_target_type>(this->culture_target)) {
+			const special_target_type target_type = std::get<special_target_type>(this->culture_target);
+			const read_only_context::scope_variant_type &target_scope_variant = ctx.get_special_target_scope_variant(target_type);
+
+			return std::visit([](auto &&target_scope) -> const culture * {
+				using target_scope_type = std::remove_const_t<std::remove_pointer_t<std::decay_t<decltype(target_scope)>>>;
+
+				if constexpr (std::is_same_v<target_scope_type, std::monostate>) {
+					assert_throw(false);
+					return nullptr;
+				} else if constexpr (std::is_same_v<target_scope_type, domain> || std::is_same_v<target_scope_type, province> || std::is_same_v<target_scope_type, site>) {
+					return target_scope->get_game_data()->get_culture();
+				} else {
+					return target_scope->get_culture();
+				}
+			}, target_scope_variant);
+		}
+
+		assert_throw(false);
+		return nullptr;
 	}
 
 private:
-	const metternich::culture *culture = nullptr;
+	target_variant<const culture> culture_target;
 };
 
 }
