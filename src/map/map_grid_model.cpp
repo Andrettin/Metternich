b94653f904e24c214d4feac1c52a90b439aa4bc4
Andrettin <6322423+Andrettin@users.noreply.github.com>
2026-08-07 01:35:51 +0200

    Improved the map grid model to have a smaller site map range (since the previous range was unnecessarily large)

--- src/map/map_grid_model.cpp
+++ src/map/map_grid_model.cpp
@@ -38,7 +38,7 @@ map_grid_model::map_grid_model()
 				}
 			}
 
-			static constexpr int site_map_range = 16;
+			static constexpr int site_map_range = 8;
 			const QRect map_block_site_rect(QPoint(map_block_start_x - site_map_range, map_block_start_y - site_map_range), defines::get()->get_map_block_size() + QSize(site_map_range * 2, site_map_range * 2));
 
 			for (const site *site : map::get()->get_sites()) {
